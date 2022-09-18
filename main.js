18ae173d92009e27e0c05967a6a98015c2f090f9
Fasbert <nikita.landar96@gmail.com>
2022-09-18 22:45:06 +0300

    first modal window

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ class HeaderTmp extends HTMLElement{
         <div class="coffee-header">
         
         </div>
-    `
-    }
+            `
+        }
     }
 
 customElements.define('header-tmp', HeaderTmp)
-
 
 //Footer Template
 class FooterTmp extends HTMLElement{
@@ -21,8 +20,22 @@ class FooterTmp extends HTMLElement{
             <h5>GitHub Page</h5>
         </a>
         </footer>
-    `
-    }
+            `
+        }
     }
 
 customElements.define('footer-tmp', FooterTmp)
+
+//Coffee Modal Page
+
+const element_espresso = document.getElementById('element-espresso');
+const modal_espresso = document.getElementById('modal-espresso');
+const close_espresso = document.getElementById('close-espresso');
+
+element_espresso.addEventListener('click', () =>{
+    modal_espresso.classList.add('show');
+})
+
+close_espresso.addEventListener('click', () =>{
+    modal_espresso.classList.remove('show');
+})
