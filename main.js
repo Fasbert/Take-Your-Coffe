dad26ef95648b9976bda6d3df7c6a168011aa20d
Fasbert <nikita.landar96@gmail.com>
2022-09-19 11:26:32 +0300

    all 8 coffee modal page complete

--- main.js
+++ main.js
@@ -67,3 +67,66 @@ close_latte.addEventListener('click', () =>{
 })
 
 // 
+const element_frappe = document.getElementById('element-frappe');
+const modal_frappe = document.getElementById('modal-frappe');
+const close_frappe = document.getElementById('close-frappe');
+
+element_frappe.addEventListener('click', () =>{
+    modal_frappe.classList.add('show');
+})
+
+close_frappe.addEventListener('click', () =>{
+    modal_frappe.classList.remove('show');
+})
+
+// 
+const element_mocha = document.getElementById('element-mocha');
+const modal_mocha = document.getElementById('modal-mocha');
+const close_mocha = document.getElementById('close-mocha');
+
+element_mocha.addEventListener('click', () =>{
+    modal_mocha.classList.add('show');
+})
+
+close_mocha.addEventListener('click', () =>{
+    modal_mocha.classList.remove('show');
+})
+
+// 
+const element_traditionalDrip = document.getElementById('element-traditional-drip');
+const modal_traditionalDrip = document.getElementById('modal-traditional-drip');
+const close_traditionalDrip = document.getElementById('close-traditional-drip');
+
+element_traditionalDrip.addEventListener('click', () =>{
+    modal_traditionalDrip.classList.add('show');
+})
+
+close_traditionalDrip.addEventListener('click', () =>{
+    modal_traditionalDrip.classList.remove('show');
+})
+
+// 
+const element_americano = document.getElementById('element-americano');
+const modal_americano = document.getElementById('modal-americano');
+const close_americano = document.getElementById('close-americano');
+
+element_americano.addEventListener('click', () =>{
+    modal_americano.classList.add('show');
+})
+
+close_americano.addEventListener('click', () =>{
+    modal_americano.classList.remove('show');
+})
+
+// 
+const element_hwt = document.getElementById('element-hwt');
+const modal_hwt = document.getElementById('modal-hwt');
+const close_hwt = document.getElementById('close-hwt');
+
+element_hwt.addEventListener('click', () =>{
+    modal_hwt.classList.add('show');
+})
+
+close_hwt.addEventListener('click', () =>{
+    modal_hwt.classList.remove('show');
+})
